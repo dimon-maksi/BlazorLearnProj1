ed94fa3da61d941ea6fd9deb0eec45629a7fbaf1
Maksimenko Dmytro <dmitriymaksimenko88@gmail.com>
2023-01-17 15:13:50 +0200

    Add project files.

---
+++ Data/Game.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorLearnProj1.Data
+{
+    public class Game
+    {
+        public string Name { get; set; }
+        public string Genre { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
